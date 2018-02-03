830cb698eafbee1dc45cb04c7b326c0e37538c02
Marisol G. Martinez <marisol.gabriela.martinez@gmail.com>
2018-02-02 22:20:39 -0800

    Let's Play

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Create an array of selection words for game
+var wordList = ["World Cup", "Super Bowl", "Masters", "U.S Open", "World Series"]
+
+//psuedo code is the liason between english and code
+// Create a variable that computer is going to hold that word (one at a time)
+//var wordStorage = ()
+//var seperateWord = wordStorage.split( '')
+//if else statements to check if the letter is word
+document.onkeyup = function (event){
+    var letterGuessed = event.key;
+} 
